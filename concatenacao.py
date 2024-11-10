2c61a5b162efcd9d6d85d0349165d0b64b0b1a88
sweetdemis <sarahpicenni3@gmail.com>
2024-11-10 12:38:46 -0300

    pequenas alteracoes

---
+++ concatenacao.py
@@ -0,0 +1,10 @@
+nota = 8.9
+s1 = "voce tirou %.1f na disciplina" %nota
+print(s1)
+
+nome1 = "pipoca"
+nome2 = "mel"
+nome3 = "amora"
+
+s2 = "meus bebes se chamam %s, %s e %s" %(nome1, nome2, nome3)
+print(s2)
